71d5604fdbdf95582249b328bf89ce8e2dd46e3f
G777h <1351964496@qq.com>
2026-05-16 17:44:48 +0800

    Update test.py

--- playground/test.py
+++ playground/test.py
@@ -1,6 +1,7 @@
 import os
 import sys
-
+import cv2
+import numpy as np
 import torch
 
 DIR = os.path.abspath(os.path.dirname(__file__))
@@ -19,6 +20,35 @@ from testing.tester_united import TesterUnited
 faulthandler.enable()
 torch.set_default_tensor_type('torch.cuda.FloatTensor')
 
+gate_counter = 0
+
+def get_gate_and_save(scale_name, save_dir="visualizations/cmgm_gates"):
+    
+    os.makedirs(save_dir, exist_ok=True)
+    
+    def hook(model, input, output):
+        global gate_counter
+       
+        feat_tensor = output.detach().cpu()
+
+       
+        for b in range(feat_tensor.size(0)):
+            feature = feat_tensor[b] 
+            
+            feat_map = torch.mean(feature, dim=0).numpy()
+
+            feat_map = feat_map - np.min(feat_map)
+            feat_map = feat_map / (np.max(feat_map) + 1e-8)
+            feat_map = np.uint8(255 * feat_map)
+
+            heatmap = cv2.applyColorMap(feat_map, cv2.COLORMAP_JET)
+
+            save_path = os.path.join(save_dir, f"{scale_name}_img{gate_counter+b}.png")
+            cv2.imwrite(save_path, heatmap)
+            print(f"Save in {save_path}")
+
+        gate_counter += feat_tensor.size(0)
+    return hook
 
 def main(argv):
     torch.backends.cudnn.deterministic = True
@@ -39,6 +69,24 @@ def main(argv):
         else:
             print("TesterUnited")
             tester = TesterUnited(args, config)
+            if hasattr(tester, 'net'):
+              print("Hooking...")
+              tester.net.g_a.rgb_analysis_transform[3].register_forward_hook(
+                 get_gate_and_save('before')
+              )
+              tester.net.g_a.rgb_analysis_transform[4].dwconv1.register_forward_hook(
+                 get_gate_and_save('scale1_small_field')
+              )
+              tester.net.g_a.rgb_analysis_transform[4].dwconv2.register_forward_hook(
+                 get_gate_and_save('scale2_medium_field')
+              )
+              tester.net.g_a.rgb_analysis_transform[4].dwconv3.register_forward_hook(
+                 get_gate_and_save('scale3_large_field')
+              )
+              tester.net.g_a.rgb_analysis_transform[5].register_forward_hook(
+                 get_gate_and_save('after')
+              )
+
     else:
         if args.model.find("master") != -1:
             print("TesterMaster")
